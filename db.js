34b6416e9c869ca97e6ec443c26c925b3bbea933
Shagor-Mia <infinityshagor18557@gmail.com>
2025-03-23 16:05:19 +0600

    db

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+
+// Cache the database connection
+let cachedConnection = null;
+
 const connectDB = async () => {
+  // If the connection is already established, reuse it
+  if (cachedConnection) {
+    return cachedConnection;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB Connected");
+    cachedConnection = connection;
+    return connection;
   } catch (err) {
     console.error("MongoDB connection error:", err);
+    throw err;
   }
 };
 
